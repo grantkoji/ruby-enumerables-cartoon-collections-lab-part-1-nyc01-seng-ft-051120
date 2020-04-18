0b16f28418b8761b74de1e291eee9007f606a326
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-18 21:26:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,6 @@ def list_dwarves(array)
   #
   # Print a numbered list of each element
   array.each_with_index do |object, i|
-    print "#{i+1}. #{{object}}"
+    print "#{i+1}. #{object}"
   end
 end
